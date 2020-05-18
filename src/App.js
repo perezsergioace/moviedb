1359098423a6e6e569d628e5757da388eb07ca7b
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-18 13:38:28 -0400

    added the search component to the project. also styled the search box component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import Search from './components/Search'
+
 function App() {
 	return (
 		<div className="App">
 			<header>
 				<h1>Movie Database</h1>
 			</header>
+      <main>
+        <Search />
+      </main>
 		</div>
 	)
 }
